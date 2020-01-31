8b8968920672ee80d81864c37ea8c7aedbc64ac5
schetininl <salexbin@bk.ru>
2020-01-31 13:55:25 +0300

    dark theme

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
+    latest = Post.objects.order_by("-pub_date")[:10]
     return render(request, "index.html", {"posts": latest})
 
 
